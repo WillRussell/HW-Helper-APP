ef17aa61552e1d935d1063540caebe9a7966c8d1
Will <wrussell35@gmail.com>
2014-10-24 16:02:31 -0400

    fixed form submission

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,4 +1,6 @@
 class ProblemsController < ApplicationController
+  # before_action :set_problem, only: [:show]
+
   def home
   end
 
@@ -9,5 +11,35 @@ class ProblemsController < ApplicationController
   def show
 
   end
+
+  def new
+  @problem = Problem.new
+  end
+
+  def create
+    @problem = Problem.new(problem_params)
+
+    respond_to do |format|
+      if @problem.save
+        format.html { redirect_to @problem, notice: 'problem was successfully created.' }
+        format.json { render :index, status: :created, location: @problem }
+      else
+        format.html { render :new }
+        format.json { render json: @problem.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_problem
+    @problem = Problem.find(params[:id])
+  end
+
+
+  def problem_params
+    params.require(:problem).permit(:title, :description)
+  end
+
 
 end
